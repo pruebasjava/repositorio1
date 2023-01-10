a8dce80f49831a9cd2247117c3027fd6d4670e4b
Jose <Jose@Portatil.Home>
2023-01-10 11:42:01 +0100

    Inicio proyecto Maven

---
+++ src/main/java/com/arquitecturajava/main/Principal.java
@@ -0,0 +1,25 @@
+package com.arquitecturajava.main;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import com.arquitecturajava.dominio.Libro;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		// Unidad de persistencia
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("biblioteca");
+		
+		
+		EntityManager em = emf.createEntityManager();
+		Libro libro = em.find(Libro.class, "1A");
+		System.out.println(libro.getIsbn());
+		System.out.println(libro.getAutor());
+		System.out.println(libro.getTitulo());
+
+	}
+
+}
